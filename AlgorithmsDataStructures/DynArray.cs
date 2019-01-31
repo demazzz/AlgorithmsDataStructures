acb622ffa8227bf72b69deefc62031888ea41e10
d.evdokimov <d.evdokimov@EVDOKIMOV>
2019-01-31 13:25:27 +0300

    DynArray First try

---
+++ AlgorithmsDataStructures/DynArray.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class DynArray<T>
+    {
+        public T[] array;
+        public int count;
+        public int capacity;
+
+        public DynArray()
+        {
+            count = 0;
+            MakeArray(16);
+        }
+
+        public void MakeArray(int new_capacity)
+        {
+            // ваш код
+            if (new_capacity < 16)
+                new_capacity = 16;
+            T[] newArray = new T[new_capacity]; //создаем новый массив заданной длины
+            if (array !=null && array.Length<new_capacity)
+            Array.Copy(array,0,newArray,0,count); //копируем все данные в него
+            if (array != null && array.Length > new_capacity)
+                Array.Copy(array, 0, newArray, 0, count + 1); //копируем все данные в него
+
+            array = newArray; //присваиваем ему имя array
+        }
+
+        public T GetItem(int index)
+        {
+            // ваш код
+            //проверка на индекс
+            if (index <= count)
+                return array[index];
+
+            else
+            {
+                throw new IndexOutOfRangeException();
+                return default(T);
+            }
+        }
+
+        public void Append(T itm)
+        {
+            // ваш код
+            if (array.Length==count)
+            {
+                MakeArray(array.Length * 2);
+            }
+            array[count++] = itm;
+        }
+
+        public void Insert(T itm, int index)
+        {
+            if (index>count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (index == count)
+            {
+                Append(itm);
+                
+            }
+            
+                if (array.Length == count)
+                {
+                    this.MakeArray(count * 2);
+                }
+                //из текущего 5 4 3 2 1 берём все с индекса 2 (3) 
+                //вставляем в индекса 2+1 длиной 3 = 5-2
+                Array.Copy(array, index, array, index + 1, count-index);
+                array[index] = itm;
+                count++;
+                // ваш код
+            
+        }
+
+        //public void Remove(int index)
+        //{
+        //    if (index >= count)
+        //    {
+        //        throw new IndexOutOfRangeException();
+        //    }
+
+        //    int shiftStart = index + 1;
+        //    if (shiftStart < count)
+        //    {
+        //        // Shift all the items following index one slot to the left.
+        //        Array.Copy(array, shiftStart, array, index, count - shiftStart);
+        //    }
+
+        //    count--;
+        //}
+        public void Remove(int index)
+        {
+            if (index > count)
+            {
+                throw new IndexOutOfRangeException();
+            }
+            if (index <= count)
+            {   //было 5 4 3 2 1
+                //стало 5 4 2 1
+                //индекс 2 (3) из исходного вектора копируется 2 и 1 в исходный массив, при этом длина 5 - 2 + 1 = 4
+                Array.Copy(array, index + 1, array, index, count - index + 1);
+            }
+            count--;
+            //Count был 33 - lenght 64, стал 32, значит надо сделать новый массив length 32
+            //проверка 32*2 > length? если да, то ничего не делаем, если нет или равно то можно уменьшать
+            if (count * 2 <= array.Length + 1 && array.Length > 16)
+            {
+                float newSize = array.Length / 1.5f;
+                int n = (int)newSize;
+                MakeArray((n));
+            }
+
+            // ваш код
+        }
+
+    }
+}
--- AlgorithmsDataStructures/Program.cs
+++ AlgorithmsDataStructures/Program.cs
@@ -12,32 +12,31 @@ namespace AlgorithmsDataStructures
     {
         static void Main(string[] args)
         {
-            LinkedList2 aaa = new LinkedList2();
-            //aaa.AddInTail(new Node(1));
-            //aaa.AddInTail(new Node(1));
-            //aaa.AddInTail(new Node(2));
-            //aaa.AddInTail(new Node(2));
-            //aaa.AddInTail(new Node(3));
-            //aaa.AddInTail(new Node(4));
-            aaa.AddInTail(new Node(1));
-            aaa.AddInTail(new Node(2));
-            aaa.AddInTail(new Node(2));
-            //aaa.AddInTail(new Node(1));
-            //aaa.AddInTail(new Node(1));
-            //aaa.AddInTail(new Node(0));
-            //aaa.AddInTail(new Node(2));
-            //aaa.AddInTail(new Node(2));
-            //aaa.AddInTail(new Node(2));
-            //aaa.AddInTail(new Node(1));
-            //aaa.AddInTail(new Node(2));
-            //aaa.AddInTail(new Node(3));
-            //aaa.AddInTail(new Node(3));
+            
+          
+            DynArray<int> aaa = new DynArray<int>();
 
-            aaa.Remove(2);
+            for (int i = 1; i <= 33; i++)
+            {
+                aaa.Append(i);
+                Console.WriteLine("{0} element and {1} is Count. Lenght = {2}", aaa.GetItem(i-1).ToString(), aaa.count.ToString(), aaa.array.Length.ToString());
+            }
+           
 
+            for (int i = 33; i < 66; i++)
+            {
+                aaa.Insert((i+1)*10, i);
+            }
+            for (int i = aaa.count; i > 0; i--)
+            {
+                
+                Console.WriteLine("{0} element and {1} is Count. Lenght = {2}", aaa.GetItem(i).ToString(), aaa.count.ToString(), aaa.array.Length.ToString());
+                aaa.Remove(i);
+                
+
+            }
+            Console.WriteLine("{0} is Count. Lenght = {1}", aaa.count.ToString(), aaa.array.Length.ToString());
             Console.ReadLine();
-
-
         }
     }
 
