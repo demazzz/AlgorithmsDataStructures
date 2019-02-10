e32912fa2cf52f6ab8ec557d0f59795b5f374c7c
d.evdokimov <d.evdokimov@EVDOKIMOV>
2019-02-10 17:38:54 +0300

    HashTable added

---
+++ AlgorithmsDataStructures/HashTable.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures
+{
+
+    public class HashTable
+    {
+        public int size;
+        public int step;
+        public string[] slots;
+
+        public HashTable(int sz, int stp)
+        {
+            size = sz;
+            step = stp;
+            slots = new string[size];
+            for (int i = 0; i < size; i++) slots[i] = null;
+        }
+
+        public int HashFun(string value)
+        {
+            // всегда возвращает корректный индекс слота
+            int tot = 0;
+            char[] cname;
+            cname = value.ToCharArray();
+            for (int i = 0; i< cname.GetUpperBound(0); i++)
+            {
+                tot += 37 * tot + (int)cname[i];
+            }
+            tot = tot % slots.GetUpperBound(0);
+            if (tot < 0)
+                tot += slots.GetUpperBound(0);
+            return tot;
+            //return 0;
+        }
+
+        public int SeekSlot(string value)
+        {
+            // находит индекс пустого слота для значения, или -1
+            int index = HashFun(value);
+            int iteration = 0;
+            if (slots[index] == null)
+            {
+                return index;
+            }
+            else
+            {
+                while (index+step < size)
+                {
+                    index += step;
+                    if (slots[index] == null)
+                    {
+                        return index;
+                    }
+                }
+                index = iteration;
+                while (iteration<size)
+                {
+                    
+                    if (slots[index]==null)
+                    {
+                        return index;
+                    }
+                    else
+                    {
+                        while (index + step < size)
+                        {
+                            index += step;
+                            if (slots[index] == null)
+                            {
+                                return index;
+                            }
+                        }
+                        iteration++;
+                        index = iteration;
+                    }
+                }
+                return -1;
+            }
+            
+        }
+
+        public int Put(string value)
+        {
+            // записываем значение по хэш-функции
+            int index = SeekSlot(value);
+            if (index != -1)
+            {
+                slots[index] = value;
+                return index;
+            }
+            else
+                return -1;
+            // возвращается индекс слота или -1
+            // если из-за коллизий элемент не удаётся разместить 
+
+        }
+
+        public int Find(string value)
+        {
+            // находит индекс слота со значением, или -1
+            int index = HashFun(value);
+            int iteration = 0;
+            if (slots[index] == value)
+                return index;
+            else
+            {
+                while (index + step < size)
+                {
+                    index += step;
+                    if (slots[index] == value)
+                    {
+                        return index;
+                    }
+                }
+                index = iteration;
+                while (iteration < size)
+                {
+                    if (slots[index] == value)
+                    {
+                        return index;
+                    }
+                    else
+                    {
+                        while (index + step < size)
+                        {
+                            index += step;
+                            if (slots[index] == value)
+                            {
+                                return index;
+                            }
+                        }
+                        iteration++;
+                        index = iteration;
+                    }
+                }
+                return -1;
+            }
+        }
+    }
+
+}
--- AlgorithmsDataStructures/Program.cs
+++ AlgorithmsDataStructures/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,38 +13,8 @@ namespace AlgorithmsDataStructures
     {
         static void Main(string[] args)
         {
-
-            OrderedList<int> aa = new OrderedList<int>(true);
-
-            //aa.Add(" AB ");
-            //aa.Add("AA ");
-
-
-            //Console.WriteLine(aa.Count());
-            //aa.Delete("AA");
-            //Console.WriteLine(aa.Count());
-
-            //Console.WriteLine(aa.Compare("AB", "AA"));
-            //Console.WriteLine(aa.Compare("AB", "AA "));
-            //Console.WriteLine(aa.Compare("AB ", "AA "));
-            //Console.WriteLine(aa.Compare(" AB", "AA"));
-            //Console.WriteLine(aa.Compare(" AB  ", "AA"));
-            //Console.WriteLine(aa.Compare(" AB ", "AA "));
-            //Console.WriteLine(aa.Compare(" AB  ", "AA "));
-            Console.WriteLine(aa.Compare(12, 1111));
-            //Console.WriteLine(aa.Compare("1", "2  "));
-            //Console.WriteLine(aa.Compare("1", " 2"));
-            //Console.WriteLine(aa.Compare("2", " 1 "));
-            //Console.WriteLine(aa.Compare("  2", "1"));
-            //Console.WriteLine(aa.Compare(" 2 ", "1"));
-            //Console.WriteLine(aa.Compare("1 ", "1                      "));
-            //Console.WriteLine(aa.Compare(" 1 ", " 1          "));
-            //Console.WriteLine(aa.Compare( "1 ", "1 "));
-            //Console.WriteLine(aa.Compare("               AB ", " AC"));
-            //Console.WriteLine(aa.Compare("AB ", " AC "));
-            //Console.WriteLine(aa.Compare(" AB ", " AC "));
-
-
+            HashTable ttt = new HashTable(19, 2);
+            
 
             Console.ReadLine(); // 0 1 2 3 3 5 5 6 8
                        
