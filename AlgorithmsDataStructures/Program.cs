145b9cce934c05f92c629e9b5d614251c3772cb9
d.evdokimov <d.evdokimov@EVDOKIMOV>
2019-03-12 21:47:24 +0300

    SimpleTree aded

--- AlgorithmsDataStructures/Program.cs
+++ AlgorithmsDataStructures/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using AlgorithmsDataStructures2;
 
 
 
@@ -15,37 +16,25 @@ namespace AlgorithmsDataStructures
         static void Main(string[] args)
         {
 
-            NativeCache<int> cache = new NativeCache<int>(11);
-            int[] arr = new int[11] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-            string[] arrS = new string[11] { "a", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K" };
-            for (int i =0;i<11;i++)
-            {
-                cache.Put(arrS[i], arr[i]);
-            }
-            Random RAND = new Random();
-            for (int i=0;i<50;i++)
-            {
-                cache.Get(arrS[RAND.Next(0, 11)]);
-            }
-            for (int i = 0; i < 11; i++)
-            {
-                Console.WriteLine("Value {0} has {1} hits", cache.values[i],cache.hits[i]);
-            }
-            cache.Put("AA", 100);
-            for (int i = 0; i < 10; i++)
-            {
-                cache.Get("AA");
-            }
-            cache.Put("CC", 300);
-            for (int i = 0; i < 10; i++)
-            {
-                cache.Get("CC");
-            }
-            cache.Put("BB", 200);
-            for (int i = 0; i < 11; i++)
-            {
-                Console.WriteLine("Value {0} has {1} hits", cache.values[i], cache.hits[i]);
-            }
+            SimpleTreeNode<int> a1 = new SimpleTreeNode<int>(10, null);
+            SimpleTreeNode<int> b1 = new SimpleTreeNode<int>(20, a1);
+            SimpleTreeNode<int> b2 = new SimpleTreeNode<int>(21, a1);
+            SimpleTreeNode<int> b3 = new SimpleTreeNode<int>(22, a1);
+            SimpleTreeNode<int> c2 = new SimpleTreeNode<int>(32, null);
+            SimpleTree<int> tree = new SimpleTree<int>(a1);
+            //tree.AddChild(a1,new SimpleTreeNode<int>(20,a1));
+            tree.AddChild(a1, b1);
+            tree.AddChild(a1, b2);
+            tree.AddChild(a1, b3);
+            tree.AddChild(b2, c2);
+            tree.MoveNode(c2, a1);
+            List<SimpleTreeNode<int>> aa = tree.GetAllNodes();
+            int aaa = tree.Count();
+            int AA = tree.LeafCount();
+            tree.DeleteNode(b2);
+            int BB = tree.LeafCount();
+            int bbb = tree.Count();
+            List<SimpleTreeNode<int>> ab = tree.GetAllNodes();
             Console.ReadLine();
         }
     }
---
+++ AlgorithmsDataStructures/SimpleTreeNode.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class SimpleTreeNode<T>
+    {
+        public T NodeValue; // значение в узле
+        public SimpleTreeNode<T> Parent; // родитель или null для корня
+        public List<SimpleTreeNode<T>> Children; // список дочерних узлов или null
+
+        public SimpleTreeNode(T val, SimpleTreeNode<T> parent)
+        {
+            NodeValue = val;
+            Parent = parent;
+            Children = null;
+        }
+    }
+
+    public class SimpleTree<T>
+    {
+        public SimpleTreeNode<T> Root; // корень, может быть null
+
+        public SimpleTree(SimpleTreeNode<T> root)
+        {
+            Root = root;
+        }
+
+        public void AddChild(SimpleTreeNode<T> ParentNode, SimpleTreeNode<T> NewChild)
+        {
+            // ваш код добавления нового дочернего узла существующему ParentNode
+            if (ParentNode.Children == null)
+            {
+                ParentNode.Children = new List<SimpleTreeNode<T>>();
+            }
+            ParentNode.Children.Add(NewChild);
+            NewChild.Parent = ParentNode;
+        }
+
+        public void DeleteNode(SimpleTreeNode<T> NodeToDelete)
+        {
+            // ваш код удаления существующего узла NodeToDelete
+            NodeToDelete.Children = null;
+            for (int i =0;i<NodeToDelete.Parent.Children.Count; i++)
+            {
+                if (Convert.ToString(NodeToDelete.NodeValue) == Convert.ToString(NodeToDelete.Parent.Children[i].NodeValue))
+                {
+                    NodeToDelete.Parent.Children.RemoveAt(i);
+                }
+            }
+            NodeToDelete.Parent = null;
+        }
+
+        public List<SimpleTreeNode<T>> GetAllNodes()
+        {
+            // ваш код выдачи всех узлов дерева в определённом порядке
+            List<SimpleTreeNode<T>> returnList = new List<SimpleTreeNode<T>>();
+            Stack<SimpleTreeNode<T>> stack = new Stack<SimpleTreeNode<T>>();
+            SimpleTreeNode<T> Node = Root;
+            stack.Push(Root);
+            while (stack.Count > 0)
+            {
+                if (Node.Children != null)
+                    foreach (SimpleTreeNode<T> node in Node.Children)
+                    {
+                    stack.Push(node);
+                    }
+                
+                Node = stack.Pop();
+                returnList.Add(Node);
+            }
+                return returnList;
+        }
+
+        public List<SimpleTreeNode<T>> FindNodesByValue(T val)
+        {
+            // ваш код поиска узлов по значению
+            List<SimpleTreeNode<T>> returnList = new List<SimpleTreeNode<T>>();
+            Stack<SimpleTreeNode<T>> stack = new Stack<SimpleTreeNode<T>>();
+            SimpleTreeNode<T> Node = Root;
+            stack.Push(Root);
+            while (stack.Count > 0)
+            {
+                if (Node.Children != null)
+                    foreach (SimpleTreeNode<T> node in Node.Children)
+                    {
+                        stack.Push(node);
+                    }
+
+                Node = stack.Pop();
+                if (Convert.ToString(Node.NodeValue) == Convert.ToString(val))
+                returnList.Add(Node);
+            }
+            return returnList;;
+        }
+
+        public void MoveNode(SimpleTreeNode<T> OriginalNode, SimpleTreeNode<T> NewParent)
+        {
+            // ваш код перемещения узла вместе с его поддеревом -- 
+            // в качестве дочернего для узла NewParent
+            AddChild(NewParent, OriginalNode);
+            OriginalNode.Parent = NewParent;
+            OriginalNode.Children = null;
+        }
+
+        public int Count()
+        {
+            // количество всех узлов в дереве
+            int count = 0;
+            Stack<SimpleTreeNode<T>> stack = new Stack<SimpleTreeNode<T>>();
+            SimpleTreeNode<T> Node = Root;
+            stack.Push(Root);
+            while (stack.Count > 0)
+            {
+                if (Node.Children != null)
+                    foreach (SimpleTreeNode<T> node in Node.Children)
+                    {
+                        stack.Push(node);
+                    }
+                Node = stack.Pop();
+                count++;
+            }
+            return count;
+        }
+
+        public int LeafCount()
+        {
+            // количество всех узлов в дереве
+            int count = 0;
+            Stack<SimpleTreeNode<T>> stack = new Stack<SimpleTreeNode<T>>();
+            SimpleTreeNode<T> Node = Root;
+            stack.Push(Root);
+            while (stack.Count > 0)
+            {
+                if (Node.Children != null)
+                    foreach (SimpleTreeNode<T> node in Node.Children)
+                    {
+                        stack.Push(node);
+                    }
+                else { count++; }
+                Node = stack.Pop();
+                
+            }
+            return count;
+        }
+
+    }
+
+}
