d03da14a2abfb3fa9de08dfff7b562eb59529f9c
d.evdokimov <d.evdokimov@EVDOKIMOV>
2019-05-16 12:32:23 +0300

    aBST first try

--- AlgorithmsDataStructures/Program.cs
+++ AlgorithmsDataStructures/Program.cs
@@ -16,37 +16,32 @@ namespace AlgorithmsDataStructures
         static void Main(string[] args)
         {
 
-            BSTNode<int> A = new BSTNode<int>(8, 8, null);
-            BSTNode<int> B = new BSTNode<int>(4, 4, null);
-            BSTNode<int> C = new BSTNode<int>(12, 12, null);
-            BSTNode<int> D = new BSTNode<int>(2, 2, null);
-            BSTNode<int> F = new BSTNode<int>(6, 6, null);
-            BSTNode<int> G = new BSTNode<int>(10, 10, null);
-            BSTNode<int> H = new BSTNode<int>(14, 14, null);
-            BSTNode<int> I = new BSTNode<int>(1, 1, null);
-            BSTNode<int> J = new BSTNode<int>(3, 3, null);
-            BSTNode<int> K = new BSTNode<int>(5, 5, null);
-            BSTNode<int> L = new BSTNode<int>(7, 7, null);
-            BSTNode<int> M = new BSTNode<int>(9, 9, null);
-            BSTNode<int> N = new BSTNode<int>(11, 11, null);
-            BSTNode<int> O = new BSTNode<int>(13, 13, null);
-            BSTNode<int> P = new BSTNode<int>(15, 15, null);
-            BST<int> tree = new BST<int>(A);
-            tree.AddKeyValue(B.NodeKey, B.NodeValue);
-            tree.AddKeyValue(C.NodeKey, C.NodeValue);
-            tree.AddKeyValue(D.NodeKey, D.NodeValue);
-            tree.AddKeyValue(F.NodeKey, F.NodeValue);
-            tree.AddKeyValue(G.NodeKey, G.NodeValue);
-            tree.AddKeyValue(H.NodeKey, H.NodeValue);
-            tree.AddKeyValue(I.NodeKey, I.NodeValue);
-            tree.AddKeyValue(J.NodeKey, J.NodeValue);
-            tree.AddKeyValue(K.NodeKey, K.NodeValue);
-            tree.AddKeyValue(L.NodeKey, L.NodeValue);
-            tree.AddKeyValue(M.NodeKey, M.NodeValue);
-            tree.AddKeyValue(N.NodeKey, N.NodeValue);
-            tree.AddKeyValue(O.NodeKey, O.NodeValue);
-            tree.AddKeyValue(P.NodeKey, P.NodeValue);
+            aBST aBST = new aBST(4);
+            int[] keys = new int[10] { 50, 25, 75, 37, 62, 84, 31, 43, 55, 92 };
+            foreach(int key in keys)
+            {
+                aBST.AddKey(key);
+            }
+            List<int?> indexes = new List<int?>();
+            int[] indexx = new int[11] { 92, 50, 37, 25, 75, 62, 84, 43, 31, 55, 81 };
+            foreach(int key in indexx)
+            {
+                indexes.Add(aBST.FindKeyIndex(key));
+            }
+            int[] add = new int[6] { 21, 19, 23, 63, 80, 1 };
+            List<int?> responces = new List<int?>();
+            foreach(int iii in add)
+            {
+                responces.Add(aBST.AddKey(iii));
+            }
+            indexes.Clear();
+            int[] indexx_final = new int[17] { 92, 50, 37, 25, 75, 62, 84, 43, 31, 55, 81,21,19,23,63,80,1 };
+            foreach(int xni in indexx_final)
+            {
+                indexes.Add(aBST.FindKeyIndex(xni));
+            }
             Console.ReadLine();
+
         }
     }
 
---
+++ AlgorithmsDataStructures/aBST.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgorithmsDataStructures2
+{
+    public class aBST
+    {
+        public int?[] Tree; // массив ключей
+
+        public aBST(int depth)
+        {
+            // правильно рассчитайте размер массива для дерева глубины depth:
+            int tree_size = Convert.ToInt32(Math.Pow(2,depth))-1;
+            Tree = new int?[tree_size];
+            for (int i = 0; i < tree_size; i++) Tree[i] = null;
+        }
+
+        public int? FindKeyIndex(int key)
+        {
+            // ищем в массиве индекс ключа
+            
+            { int i = 0;
+                while(i<Tree.Length)
+                { if (Tree[i] != null)
+                    {
+                        if (Tree[i] > key)
+                            i = 2 * i + 1;
+                        else if (Tree[i] < key)
+                            i = 2 * i + 2;
+                        else return i;
+                    }
+                    else
+                    {
+                        i = i * -1;
+                        return i;
+                    }
+                }
+            }
+            return null; // не найден
+        }
+
+        public int AddKey(int key)
+        {
+            // добавляем ключ в массив
+
+            //while(Tree[i]!=null)
+            //{
+            //    if (Tree[i] > key)
+            //        i = 2 * i +1;
+            //    else
+            //        i = 2 * i + 2;
+            //}
+            int? k = FindKeyIndex(key);
+            if (k != null)
+            {
+                int i = Convert.ToInt32(k);
+                if (i < 0 && i < Tree.Length)
+                {
+
+                    Tree[i * -1] = key;
+                    
+                }
+                if (i==0)
+                {
+                    Tree[i] = key;
+                }
+                return Convert.ToInt32(i);
+            }
+            else
+                return -1;
+            // индекс добавленного/существующего ключа или -1 если не удалось
+        }
+
+    }
+}
