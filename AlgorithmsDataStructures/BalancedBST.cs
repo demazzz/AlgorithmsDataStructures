398a1109acd2170d415792f17c96af0dacc3f21e
d.evdokimov <d.evdokimov@EVDOKIMOV>
2019-07-03 12:38:22 +0300

    syncup

--- AlgorithmsDataStructures/BalancedBST.cs
+++ AlgorithmsDataStructures/BalancedBST.cs
@@ -13,17 +13,18 @@ namespace AlgorithmsDataStructures2
                 int lenght = Convert.ToInt32(Math.Pow(2, ((Math.Ceiling(Math.Log(a.Length) / Math.Log(2))))) - 1); //calculate the lenght
                 int[] final = new int[lenght]; //build return array
                 Array.Sort(a); //ascending sort
-                final = FindElements(a, final, 0,lenght);
+                int layer = 0;
+                final = FindElements(a, final, 0,layer);
                 return final;
             }
             else return null;
         }
-        public static int[] FindElements(int[] a, int[] result, int pos, int lenght)
+        public static int[] FindElements(int[] a, int[] result, int pos, int layer)
         {            
             if (a.Length > 1)
                 result[pos] = a[a.Length / 2];
             else if (a.Length==1) result[pos] = a[0];
-            pos++;
+            if (Math.Pow(2, layer) + 1 == pos) layer++;
             if (a.Length > 1)
             {
                 
@@ -31,8 +32,13 @@ namespace AlgorithmsDataStructures2
                 int[] right = new int[a.Length / 2];
                 Array.Copy(a, 0, left, 0, a.Length / 2);
                 Array.Copy(a, a.Length / 2, right, 0, a.Length / 2);
-                FindElements(left, result, pos, lenght);
-                FindElements(right, result, pos, lenght);
+                if (Math.Pow(2, layer) + 1 < pos - 1)
+                { pos++;
+                FindElements(left, result, pos, layer); 
+                pos++;
+                    FindElements(right, result, pos, layer);
+                }
+
             }
             return result;
         }
