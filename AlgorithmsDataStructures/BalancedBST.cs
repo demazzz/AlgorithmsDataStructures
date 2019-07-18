045a6dada7dc139aac180082b936af3083d04018
Дмитрий Евдокимов <demazzz@yandex.ru>
2019-07-18 14:23:09 -0400

    First Try

--- AlgorithmsDataStructures/BalancedBST.cs
+++ AlgorithmsDataStructures/BalancedBST.cs
@@ -13,34 +13,54 @@ namespace AlgorithmsDataStructures2
                 int lenght = Convert.ToInt32(Math.Pow(2, ((Math.Ceiling(Math.Log(a.Length) / Math.Log(2))))) - 1); //calculate the lenght
                 int[] final = new int[lenght]; //build return array
                 Array.Sort(a); //ascending sort
-                int layer = 0;
-                final = FindElements(a, final, 0,layer);
+                Queue<Node> queue = new Queue<Node>();
+                Node root = sortedArrayToBST(a, 0, a.Length - 1);
+                int i = 1;
+                queue.Enqueue(root);
+                final[0] = root.data;
+                while (queue.Count!=0)
+                {
+                    Node node = queue.Dequeue();
+                    if (node.left != null)
+                    {
+                        final[i++] = node.left.data;
+                        queue.Enqueue(node.left);
+                    }
+                    if (node.right != null)
+                    {
+                        final[i++] = node.right.data;
+                        queue.Enqueue(node.right);
+                    }
+                }
                 return final;
             }
             else return null;
         }
-        public static int[] FindElements(int[] a, int[] result, int pos, int layer)
-        {            
-            if (a.Length > 1)
-                result[pos] = a[a.Length / 2];
-            else if (a.Length==1) result[pos] = a[0];
-            if (Math.Pow(2, layer) + 1 == pos) layer++;
-            if (a.Length > 1)
-            {
-                
-                int[] left = new int[a.Length / 2];
-                int[] right = new int[a.Length / 2];
-                Array.Copy(a, 0, left, 0, a.Length / 2);
-                Array.Copy(a, a.Length / 2, right, 0, a.Length / 2);
-                if (Math.Pow(2, layer) + 1 < pos - 1)
-                { pos++;
-                FindElements(left, result, pos, layer); 
-                pos++;
-                    FindElements(right, result, pos, layer);
-                }
 
+        public static Node sortedArrayToBST(int[] arr, int start, int end)
+        { 
+            if (start > end)
+            {
+                return null;
             }
-            return result;
+            int mid = (start + end) / 2;
+            Node node = new Node(arr[mid]);
+            node.left = sortedArrayToBST(arr, start, mid - 1);
+            node.right = sortedArrayToBST(arr, mid + 1, end);
+            return node;
+        }
+        public class Node
+        {
+
+            public int data;
+            public Node left, right;
+
+            public Node(int d)
+            {
+                data = d;
+                left = right = null;
+            }
         }
     }
+
 }
