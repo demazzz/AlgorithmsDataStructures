f7c226a0ac35926378f665386dcaf2743b4a9193
Дмитрий Евдокимов <demazzz@yandex.ru>
2019-09-20 08:17:59 -0400

    SimpleGraph first try

--- AlgorithmsDataStructures/Program.cs
+++ AlgorithmsDataStructures/Program.cs
@@ -16,17 +16,55 @@ namespace AlgorithmsDataStructures
         static void Main(string[] args)
         {
 
-           int []arr = { 1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17,2,124,244,11,111 };  
-           int n = arr.Length;  
-           Heap heap = new Heap();
+           //int []arr = { 1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17,2,124,244,11,111 };  
+           //int n = arr.Length;
+            //SimpleGraph graph = new SimpleGraph(4);
+            //graph.AddVertex(10);
+            //graph.AddVertex(15);
+            //graph.AddVertex(21);
+            //graph.AddVertex(23);
 
-            
-            heap.MakeHeap(arr, 1);
-            heap.MakeHeap(arr, 2);
-            heap.MakeHeap(arr, 3);
-            int test1 = heap.GetMax();
+            //graph.AddEdge(0, 1);
+            //graph.AddEdge(0, 2);
+            //graph.AddEdge(1, 2);
+            //graph.AddEdge(0, 3);
+            //graph.AddEdge(2, 3);
+            //graph.output();
+            //graph.RemoveEdge(0, 3);
+            //graph.output();
+            //graph.AddVertex(30);//nothing should change
+            //graph.output();
+            //graph.AddEdge(1,1);
+            //graph.output();
+            //graph.RemoveVertex(1);
+            //graph.output();
 
-            Console.ReadLine();
+            SimpleGraph graph = new SimpleGraph(4);
+
+            int[] arr = { 10, 15, 21, 23 };
+
+            foreach (int item in arr)
+            {
+                graph.AddVertex(item);
+                for (int i = 0; i < graph.max_vertex; i++)
+                {
+                    if (graph.vertex[i] != null)
+                    {
+                        if (graph.vertex[i].Value == item)
+                        {
+                            for (int j = 0; j < graph.max_vertex; j++)
+                            {
+                                if (graph.IsEdge(i, j)) Console.WriteLine("FAIL");
+                                else Console.WriteLine("no edges !!!!!!!");
+                            }
+
+                        }
+                    }
+                }
+
+            }
+
+                Console.ReadLine();
 
         }
 
---
+++ AlgorithmsDataStructuresTests2/SimpleGraphTests.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgorithmsDataStructures2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsDataStructures2.Tests
+{
+    [TestClass()]
+    public class SimpleGraphTests
+    {
+        SimpleGraph graph = new SimpleGraph(4);
+        [TestMethod()]
+        public void SimpleGraphTest()
+        {
+
+            //SimpleGraph graph = new SimpleGraph(4);
+
+            int[] arr = { 10, 15, 21, 23 };
+
+            foreach (int item in arr)
+            {
+                graph.AddVertex(item);
+                for (int i = 0; i < graph.max_vertex; i++)
+                {
+                    if (graph.vertex[i] != null)
+                    {
+                        if (graph.vertex[i].Value == item)
+                        {
+                            for (int j = 0; j < graph.max_vertex; j++)
+                            {
+                                if (graph.IsEdge(i, j)) Assert.Fail();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        [TestMethod()]
+        public void AddEdge()
+        {
+            int[] arr = { 10, 15, 21, 23 };
+
+            foreach (int item in arr)
+            {
+                graph.AddVertex(item);
+            }
+            int[] j = { 0, 0, 1, 0, 2 };
+            int[] k = { 1, 2, 2, 3, 3 };
+            for (int i1 = 0; i1 < j.Length; i1++)
+                if (!graph.IsEdge(j[i1], k[i1]))
+                {
+                    graph.AddEdge(j[i1], k[i1]);
+                    if (!graph.IsEdge(j[i1], k[i1]))
+                        Assert.Fail();
+                }
+                else Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void removeEdge()
+        {
+            int[] arr = { 10, 15, 21, 23 };
+
+            foreach (int item in arr)
+            {
+                graph.AddVertex(item);
+            }
+            int[] j = { 0, 0, 1, 0, 2 };
+            int[] k = { 1, 2, 2, 3, 3 };
+            for (int i1 = 0; i1 < j.Length; i1++)
+                if (!graph.IsEdge(j[i1], k[i1]))
+                {
+                    graph.AddEdge(j[i1], k[i1]);
+                    if (!graph.IsEdge(j[i1], k[i1]))
+                        Assert.Fail();
+                }
+                else Assert.Fail();
+
+            if (!graph.IsEdge(0, 3)) Assert.Fail();
+            graph.RemoveEdge(0, 3);
+            if (graph.IsEdge(0, 3)) Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void deleteverted()
+        {
+            int[] arr = { 10, 15, 21, 23 };
+
+            foreach (int item in arr)
+            {
+                graph.AddVertex(item);
+            }
+            int[] j = { 0, 0, 1, 0, 2 };
+            int[] k = { 1, 2, 2, 3, 3 };
+            for (int i1 = 0; i1 < j.Length; i1++)
+                if (!graph.IsEdge(j[i1], k[i1]))
+                {
+                    graph.AddEdge(j[i1], k[i1]);
+                    if (!graph.IsEdge(j[i1], k[i1]))
+                        Assert.Fail();
+                }
+                else Assert.Fail();
+
+            if (!graph.IsEdge(0, 3)) Assert.Fail();
+            graph.RemoveEdge(0, 3);
+            if (graph.IsEdge(0, 3)) Assert.Fail();
+
+            int edgecount = 0;
+            for (int i2=0;i2<graph.max_vertex;i2++)
+            {
+                if (!graph.IsEdge(1, i2))
+                    edgecount++;
+            }
+            if (edgecount == graph.max_vertex) Assert.Fail();
+                graph.RemoveVertex(1);
+             edgecount = 0;
+            for (int i2 = 0; i2 < graph.max_vertex; i2++)
+            {
+                if (!graph.IsEdge(1, i2))
+                    edgecount++;
+            }
+            if (edgecount != graph.max_vertex ) Assert.Fail();
+            graph.output();
+            
+        }
+
+      
+    }
+}
